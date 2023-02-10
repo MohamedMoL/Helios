98b927bd96fd7ee8f3c8e9f6a8bded974bd649e4
QTØ <qtnull@nekoc.cc>
2023-02-10 22:02:47 +0000

    (Untested) Added code for sensor BMP280 and SI1145

--- Arduino/NyanSat/DataModels.h
+++ Arduino/NyanSat/DataModels.h
@@ -1,5 +1,11 @@
 struct SensorData {
+  // Onboard timer
   long time;
+  // BMP280 (Pressure, temperature)
   float temperature;
   float pressure;
+  // SI1145 (Visible Light, Infrared light, UV Index)
+  int visibleLight;
+  int infraredLight;
+  float UVIndex;
 };
--- Arduino/NyanSat/NyanSat.ino
+++ Arduino/NyanSat/NyanSat.ino
@@ -12,28 +12,47 @@ Project/Proyecto Hermes (Internal Tracker: NyanSat)
 // Importing libraries / Importación de librerías
 #include "DataModels.h"
 #include <Adafruit_BMP280.h>
+#include <Adafruit_SI1145.h>
 
 Adafruit_BMP280 bmp;
+Adafruit_SI1145 uv = Adafruit_SI1145();
+
+// Both of these Adafruit classes seems to use the same "Wire" object? Should I create them here and pass it in as a parameter?
 
 SensorData collectSensorData() {
-  // Collects sensor data from all connected sensors and processes them / Recolecta datos de cada sensor y los procesa
+  /* Collects sensor data from all connected sensors and processes them / Recolecta datos de cada sensor y los procesa */
+  // Skeleton / Esqueleto
   SensorData currentSensorData;
+  
+  // Onboard timer / temporizador
   currentSensorData.time = millis();
   
+  // BMP280: Temperature, pressure / BMP280: Temperatura, presión
   currentSensorData.temperature = bmp.readTemperature();
   currentSensorData.pressure = bmp.readPressure();
   
+  // SI1145
+  currentSensorData.visibleLight = uv.readVisible();
+  currentSensorData.infraredLight = uv.readIR();
+  currentSensorData.UVIndex = uv.readUV() / 100.0;
   return currentSensorData;
 }
 
 void setup() {
+  /* Initialize Remote Transmission */
   Serial.begin(9600);
+
+  /* Sensor Setup */
+  // BMP280
   bmp.begin();
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
                   Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  // SI1145
+  uv.begin();
+
 }
 
 void loop() {
